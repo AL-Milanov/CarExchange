223ed2168907ce48a257d384edd419e1769fbd7f
AL-Milanov <alexandermilanov161195@gmail.com>
2022-05-18 13:25:29 +0300

    models for car service

---
+++ CarExchange.Core/Models/AllCarsVM.cs
@@ -0,0 +1,15 @@
+﻿namespace CarExchange.Core.Models
+{
+    public class AllCarsVM
+    {
+        public string Id { get; set; }
+
+        public string Car { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Mileage { get; set; }
+
+        public byte[]? Picture { get; set; }
+    }
+}
---
+++ CarExchange.Core/Models/CarResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace CarExchange.Core.Models
+{
+    public class CarResponse
+    {
+        public List<AllCarsVM> Cars{ get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int Pages { get; set; }
+
+        public CarResponse()
+        {
+            Cars = new List<AllCarsVM>();
+        }
+    }
+}
