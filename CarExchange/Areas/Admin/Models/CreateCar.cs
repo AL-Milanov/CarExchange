19f67d09abe8a2c39f7cebec5d6b8900af2e2fba
AL-Milanov <alexandermilanov161195@gmail.com>
2022-07-05 16:29:41 +0300

    added data specifications

--- CarExchange/Areas/Admin/Models/CreateCar.cs
+++ CarExchange/Areas/Admin/Models/CreateCar.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarExchange.Areas.Admin.Models
 {
     public class CreateCar
     {
+        [StringLength(20, MinimumLength = 2)]
         public string? Model { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int Mileage { get; set; }
 
+        [Range(1, 9)]
         public byte Seats { get; set; }
 
+        [Range(1, 6)]
         public byte Gears { get; set; }
 
+        [Range(1, double.MaxValue)]
         public decimal Price { get; set; }
 
+        [StringLength(750)]
         public string? Description { get; set; }
 
         public string BodyType { get; set; }
